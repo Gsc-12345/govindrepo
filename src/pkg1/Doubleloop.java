e635759f718917d7b13a31ae0173bd6a1660a7af
Govind Chauhan <Govind Chauhan@DESKTOP-392F33V>
2022-11-14 17:54:52 +0530

    Add

--- src/pkg1/Doubleloop.java
+++ src/pkg1/Doubleloop.java
@@ -3,21 +3,18 @@ package pkg1;
 public class Doubleloop 
 {
 	public static void main(String[] args) 
-	{ int count=1;
-		for(int i=1; i<=4; i++)
-		{
-			for(int j=1; j<=i; j++)
-			{
-				 {
-				System.out.print(count+" ");
-				count++;}
-				
-				
-		}System.out.println();
-			
-		
-		
+	{        
+	for (int i=0; i<6; i++)   
+	{  
+	for (int j=6-i; j>1; j--)   
+	{  
+	System.out.print(" ");   
+	}   
+	for (int j=0; j<=i; j++ )   
+	{       
+	System.out.print("* ");   
 	}
-
-}
-}
+	System.out.println();   
+	}   
+	}   
+	}  
